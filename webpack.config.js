53816d22ef89fb2f2f86078984c8eb9479f1411f
OlyaKorabliova <korablyova@ucu.edu.ua>
2018-02-24 19:24:31 +0200

    Redux: Writing a Counter Reduces with Tests

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    entry: './src/Counter.js',
+    output: {
+        filename: './build/bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.less$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "less-loader" // compiles Less to CSS
+                }]
+            }
+        ]
+    }
+};
