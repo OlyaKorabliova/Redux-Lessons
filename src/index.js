61b064655481003b4c3902eeeb3092fd93be1e9e
OlyaKorabliova <korablyova@ucu.edu.ua>
2018-03-04 19:40:58 +0200

    Redux: Refactoring the Entry Point.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import todoApp from "./reducers";
-import {createStore} from "redux";
-import {Provider} from "react-redux"
-import {loadState, saveState} from "./localStorage"
-import {App} from "./components/App"
-import throttle from "lodash/throttle";
-
-const persistedState = loadState();
-const store = createStore(
-    todoApp,
-    persistedState
-);
-
-store.subscribe(throttle(() => {
-    saveState({
-        todos: store.getState().todos
-    });
-}, 1000));
+import configureStore from "./configureStore"
+import Root from "./components/Root"
+const store = configureStore();
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
+    <Root store={store} />,
     document.getElementById("root")
 );
