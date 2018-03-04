fc403fffc842ad90b1b260f30f38eb54e095dab8
OlyaKorabliova <korablyova@ucu.edu.ua>
2018-03-04 20:11:25 +0200

    Redux: Adding React Router to the Project.

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,11 +1,19 @@
 import React from "react"
+import PropTypes from "prop-types"
 import {Provider} from "react-redux"
 import {App} from "./App"
+import {Router, Route, browserHistory} from "react-router"
 
-const Root = ({store}) => (
+const Root = ({ store }) => (
     <Provider store={store}>
-        <App />
+        <Router history={browserHistory}>
+            <Route path='/' component={App} />
+        </Router>
     </Provider>
 );
+
+Root.propTypes = {
+    store: PropTypes.object.isRequired
+};
 
 export default Root;
